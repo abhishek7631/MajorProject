3c0e08c21fba58c2f674c4decc5c6da449a92282
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-20 12:53:21 +0530

    validation for schema

--- app.js
+++ app.js
@@ -32,6 +32,16 @@ app.get("/", (req, res) => {
   res.send("hello, i'm root");
 });
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
 //Index Route
 
 app.get(
@@ -63,12 +73,8 @@ app.get(
 
 app.post(
   "/listings",
+  validateListing,
   wrapAsync(async (req, res, next) => {
-    let result = listingSchema.validate(req.body);
-    // console.log(result);
-    if (result.error) {
-      throw new ExpressError(400, result.error);
-    }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -90,10 +96,8 @@ app.get(
 
 app.put(
   "/listings/:id",
+  validateListing,
   wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-      throw new ExpressError(404, "Send valid data for listing");
-    }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
