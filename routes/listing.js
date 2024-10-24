0edbc801b14fc801a4358c62c91b284d40b64d7b
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-24 10:34:32 +0530

    flash success includes

--- routes/listing.js
+++ routes/listing.js
@@ -74,6 +74,7 @@ router.put(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
   })
 );
@@ -86,6 +87,7 @@ router.delete(
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
   })
 );
