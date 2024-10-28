cd0acf670ecb78acf825625b2bcc11fa42e17edb
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-28 12:03:08 +0530

    store files

--- routes/listing.js
+++ routes/listing.js
@@ -5,7 +5,8 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 //Index Route and Create Route
 router
