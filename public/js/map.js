5f5e774fd8f2cc8793d8e875541bc78fe5804852
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-29 14:21:00 +0530

    storing coordinates

--- public/js/map.js
+++ public/js/map.js
@@ -4,3 +4,7 @@ const map = new mapboxgl.Map({
   center: [77.2088, 28.6139], // starting position [lng, lat].
   zoom: 9,
 });
+
+const marker = new mapboxgl.Marker()
+  .setLngLat([12.554729, 55.70651])
+  .addTo(map);
