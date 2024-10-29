de9bc4e66ea25718bef5358d101a18c47faf73a8
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-29 14:57:51 +0530

    map marker

--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,15 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
-  center: [77.2088, 28.6139], // starting position [lng, lat].
+  center: listing.geometry.coordinates, // starting position [lng, lat].
   zoom: 9,
 });
 
-const marker = new mapboxgl.Marker()
-  .setLngLat([12.554729, 55.70651])
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates) //Listing.geometry.coordinates
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${listing.title}</h4><p>Exact Loaction will be provided after booking</p>`
+    )
+  )
   .addTo(map);
